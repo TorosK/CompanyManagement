fd1a8471a199f5c8b8eb27c43278a32a6853657a
farrukhb30 <78017890+farrukhb30@users.noreply.github.com>
2021-02-16 13:18:29 +0100

    Farrukh

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,16 +1,12 @@
 
 package ui;
 
-import java.util.Locale;
-
 
 public class Main {
 
 
     public static void main(String[] args) {
             
-//            Locale.setDefault(Locale.GERMAN);
-//            System.setProperty("file.encoding", "UTF-8");
             UI.runProgram();
             
     }
